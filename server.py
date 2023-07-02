33227bd5639470206d19bc03901aa130e5ce98ac
Anthony <anthonyargatoff@gmail.com>
2023-07-02 14:20:59 -0700

    added negative loop output

--- server.py
+++ server.py
@@ -1,16 +1,20 @@
 import time
 from scraping import *
 from send_email import *
+import keyboard
 
 starttime = time.time()
 recipients = ["anthonyargatoff@gmail.com", "mrkoolpants@gmail.com", "emily.denison3@gmail.com"]
-print("Server is running.")
+print("Server is running. Press 'q' to quit.")
+time_interval = 60 # time is in seconds
 
 while True:
-    time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+    time.sleep(time_interval - ((time.time() - starttime) % time_interval))
     coop_bool, coop_string = get_vacancies()
-
     if (coop_bool):
         body = "One or more coops are available. View below:\n{}View the website here: https://co-ophousingtoronto.coop/resources/find-a-coop/".format(
             coop_string)
         send_email("Coop Available!", body, recipients)
+    else:
+        print("No co-op vacancies found. Time next search in {} seconds.".format(time_interval))
+    
