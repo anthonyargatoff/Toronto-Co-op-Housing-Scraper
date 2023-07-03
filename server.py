3d8f8f27f5d152ad01731ae44776ea0a935b2ce3
Anthony Argatoff <122230360+anthonyargatoff@users.noreply.github.com>
2023-07-02 19:32:55 -0700

    changed to minutes

--- server.py
+++ server.py
@@ -4,11 +4,11 @@ from send_email import *
 
 starttime = time.time()
 recipients = ["anthonyargatoff@gmail.com", "mrkoolpants@gmail.com", "emily.denison3@gmail.com"]
-time_interval = int(input("Enter search frequency in seconds:"))
-print("Server is running with frequency of {} seconds. Press 'q' to quit.".format(time_interval))
+time_interval = int(input("Enter search frequency in minutes:"))
+print("Server is running with frequency of {} minutes. Press 'q' to quit.".format(time_interval))
 
 while True:
-    time.sleep(time_interval - ((time.time() - starttime) % time_interval))
+    time.sleep((time_interval * 60 ) - ((time.time() - starttime) % (60 *time_interval)))
     coop_bool, coop_string = get_vacancies()
     if (coop_bool):
         body = "One or more coops are available. View below:\n{}View the website here: https://co-ophousingtoronto.coop/resources/find-a-coop/".format(
@@ -17,5 +17,5 @@ while True:
         print("Co-op vacancy found. Sending email to {}.\nNext Update in 12 hours\n".format(recipients))
         time.sleep(43200)
     else:
-        print("No co-op vacancies found. Next search in {} seconds.".format(time_interval))
+        print("No co-op vacancies found. Next search in {} minutes.".format(time_interval))
     
