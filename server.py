1e4f52133aae155a48719912f7560ca71fee7bba
Anthony <anthonyargatoff@gmail.com>
2023-07-05 09:18:56 -0700

    changed order of total times checked. Added docstr

--- server.py
+++ server.py
@@ -4,36 +4,60 @@ from scraping import *
 from send_email import *
 
 def get_time():
+    """
+    Gets full date in yyyy/mm/dd hh:mm format
+
+    Returns:
+        _type_: string
+    """    
     now = datetime.now()
     current_time = now.strftime("Date: %Y/%m/%d Time: %H:%M")
     return current_time
 
 def get_week_day():
+    """
+    Gets the Day of the week in a string
+
+    Returns:
+        _type_: string
+    """    
     now = datetime.now()
     week_day = now.strftime("%A")
     return week_day
 
 def check_time():
+    """
+    Return the time in 24 hour clock format of %H
+
+    Returns:
+        _type_: int
+    """    
     now = datetime.now()
     check_time = now.strftime("%-H")
     check_time = int(check_time)
     return check_time
 
-def get_email():
-    email_file = open("email_addresses.txt", "r")
+def get_email(x):
+    """
+    Create a list of emails from the email_addresses.txt file
+
+    Args:
+        x (_type_): list of strings
+    """     
+    email_file = open("./email_addresses.txt", "r")
     for email in email_file:
         if email.startswith("#"):
          None
         else:
             email = email.replace("\n", "")
-            recipients.append(email)
+            x.append(email)
     email_file.close()
     
 recipients = []
-get_email()
+get_email(recipients)
 times_checked_weekly = 0
 
-open_total_times = open("total_times_checked.txt", "r" )
+open_total_times = open("./total_times_checked.txt", "r" )
 times_checked_total = int(open_total_times.read())
 open_total_times.close()
 
@@ -49,14 +73,16 @@ while True:
     coop_bool, coop_string = get_vacancies()    
     
     if ( get_week_day() == "Sunday" and check_time() >= 9 and check_sent_admin_email == False):
+        
+        times_checked_total += times_checked_weekly
         admin_body = "Here is the weekly report:\nInterval of search: {} minutes\nTimes searched this week: {}\nTotal times searched: {}\nCo-op vacancies found this week: {}\nNo co-op vacancies found this week: {}\nSent at {} PST".format(time_interval, times_checked_weekly, times_checked_total, positive_results, negative_results, get_time())
         send_email("Toronto Co-op Housing Search: Weekly Report", admin_body, recipients)
         check_sent_admin_email = True
-        times_checked_total += times_checked_weekly
+        
         open_total_times = open("total_times_checked.txt", "w")
         open_total_times.write(times_checked_total)
         open_total_times.close        
-        negative_results, positive_results = 0
+        negative_results, positive_results, times_checked_weekly = 0
     
     if( check_sent_admin_email == True and get_week_day() != "Sunday"):
         check_sent_admin_email = False
