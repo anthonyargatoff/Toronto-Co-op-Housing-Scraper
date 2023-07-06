815b288fc2e098c245d2ca47fee11032171a9aed
Anthony <anthonyargatoff@gmail.com>
2023-07-05 19:29:37 -0700

    created docstring

--- send_email.py
+++ send_email.py
@@ -2,7 +2,15 @@ import smtplib
 from email.mime.text import MIMEText
 
 
-def send_email(subject, body, recipients):
+def send_email(subject:str, body:str, recipients:list[str]):
+    """
+    Sends an email with subject, body and recipients
+
+    Args:
+        subject (string): Email subject
+        body (string): Email body
+        recipients (list[str]): A list of email addresses to be sent to.
+    """    
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['From'] = "autoanthonyemailer@gmail.com"
