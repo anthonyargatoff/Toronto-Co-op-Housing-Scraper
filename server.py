548662aeb4db77fe250ee7b7205c625565ffc2ef
Anthony <anthonyargatoff@gmail.com>
2023-07-05 18:46:23 -0700

    added tracking for positive and negatives

--- server.py
+++ server.py
@@ -3,6 +3,17 @@ from datetime import datetime
 from scraping import *
 from send_email import *
 
+def create_txt_files():
+    """
+    Creates required text files
+    """    
+    file1 = open('log.txt', 'r+')
+    file1.close()
+    file2 = open('email_addresses.txt', 'r+')
+    file2.close()
+    file3 = open('counter.txt', 'r+')
+    file3.close()
+
 def get_time():
     """
     Gets full date in yyyy/mm/dd hh:mm format
@@ -65,16 +76,45 @@ def write_log(message):
     print(message)
     file.close
 
+def edit_counter(total, total_positive, total_negative):
+    """
+    Adds the new totals to the counter.txt file
+
+    Args:
+        total (_type_): int
+        total_positive (_type_): int
+        total_negative (_type_): int
+
+    Returns:
+        _type_: _description_
+    """    
+    with open("./counter.txt", 'r') as f:
+        lines = f.readlines()
+        f.close()
+    
+    lines[0], lines[1], lines[2] = (str(total) + "\n"), (str(total_positive) + "\n"), (str(total_negative) + "\n") 
+    
+    with open("./counter.txt", "w") as f:
+        f.writelines(lines)
+        f.close()
+        
+    return None
+    
+create_txt_files()
 recipients = []
 get_email(recipients)
+
+
+open_counter = open("./counter.txt", "r" )
+counter = open_counter.readlines()
+total_times_checked = int(counter[0])
+total_negative_results = int(counter[2])
+total_positive_results = int(counter[1])
+open_counter.close()
+
 times_checked_weekly = 0
-
-open_total_times = open("./total_times_checked.txt", "r" )
-times_checked_total = int(open_total_times.read())
-open_total_times.close()
-
-negative_results = 0
-positive_results = 0
+weekly_positive_results = 0
+weekly_negative_results = 0
 check_sent_admin_email = False
 
 starttime = time.time()
@@ -86,16 +126,16 @@ while True:
     
     if ( get_week_day() == "Sunday" and check_time() >= 9 and check_sent_admin_email == False):
         
-        times_checked_total += times_checked_weekly
-        admin_body = "Here is the weekly report:\nInterval of search: {} minutes\nTimes searched this week: {}\nTotal times searched: {}\nCo-op vacancies found this week: {}\nNo co-op vacancies found this week: {}\nSent at {} PST".format(time_interval, times_checked_weekly, times_checked_total, positive_results, negative_results, get_time())
+        total_times_checked += times_checked_weekly
+        total_negative_results += weekly_negative_results
+        total_positive_results += weekly_positive_results
+        
+        admin_body = "Here is the weekly report:\nInterval of search: {} minutes\nTimes searched this week: {}\nCo-op vacancies found this week: {}\nNo co-op vacancies found this week: {}\nTotal amount of co-ops found: {}\nTotal amount of co-ops not found: {}\nTotal times searched: {}\nPercentage of co-ops found: {}%\nSent at {} PST".format(time_interval, times_checked_weekly, weekly_positive_results, weekly_negative_results, total_positive_results, total_negative_results, total_times_checked, total_positive_results/total_times_checked, get_time())
         send_email("Toronto Co-op Housing Search: Weekly Report", admin_body, recipients)
         check_sent_admin_email = True
         
-        open_total_times = open("total_times_checked.txt", "w")
-        open_total_times.write(times_checked_total)
-        open_total_times.close        
-        negative_results, positive_results, times_checked_weekly = 0
-        
+        edit_counter(total_times_checked, total_positive_results, total_negative_results)
+        weekly_negative_results, weekly_positive_results, times_checked_weekly = 0
         write_log("Weekly report sent at {}".format(get_time()))
     
     if( check_sent_admin_email == True and get_week_day() != "Sunday"):
@@ -110,13 +150,13 @@ while True:
         send_email("Toronto Co-op Housing Search: Coop Available!", body, recipients)
         write_log("System Time: {}. Co-op vacancy found. Sending email to {}. Next Update in 12 hours\n".format(get_time(), recipients))
 
-        positive_results += 1
+        weekly_positive_results += 1
         times_checked_weekly += 1
         time.sleep(43200)
     else:
         
         write_log("System: {}. No co-op vacancies found. Next search in {:.0f} minutes.".format(get_time(), time_interval))
         times_checked_weekly += 1
-        negative_results += 1
+        weekly_negative_results += 1
         time.sleep((time_interval * 60 ) - ((time.time() - starttime) % (60 *time_interval)))
     
