8a25a6b72462b62b2ade0e7856a0e968fa838dcf
anthonyargatoff <anthonyargatoff@gmail.com>
2025-02-16 15:35:31 -0800

    Use execute script

--- sqlite.py
+++ sqlite.py
@@ -1,51 +1,44 @@
 import sqlite3
 
+
 def createSchema():
     try:
         con = sqlite3.connect("toronto.db")
         cur = con.cursor()
 
-        cur.execute(
+        cur.executescript(
             """
             CREATE TABLE availability(
                 id INTEGER PRIMARY KEY,
                 status TEXT
             );
-            """
-        )
-        cur.execute(
-            """
+
             CREATE TABLE coop(
                 id INTEGER PRIMARY KEY,
                 name TEXT,
-                email TEXT,
                 availability INTEGER,
                 FOREIGN KEY (availability) REFERENCES availability(id) ON UPDATE CASCADE ON DELETE SET NULL
             );
-            """
-        )
-        cur.execute(
-            """
+
             CREATE TABLE coopAvailabilities(
                 id INTEGER PRIMARY KEY,
                 coopId INTEGER,
                 startAvailability TEXT DEFAULT CURRENT_TIMESTAMP,
                 endAvailability TEXT
             );
-            """
-        )
-        cur.execute(
-            """
+
             CREATE TABLE statistics(
                 id INTEGER PRIMARY KEY,
                 totalCount INTEGER,
                 weeklyCount INTEGER
             );
+
+            INSERT INTO availability(status) VALUES('vacancies', 'waiting_list', 'no_vacancies');
             """
         )
-        cur.execute("INSERT INTO availability(status) VALUES('vacancies', 'waiting_list', 'no_vacancies')")
     except Exception as error:
         print(error)
-    
+
+
 if __name__ == "__main__":
     createSchema()
