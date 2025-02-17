9e59f0df1f8447585e2287be2125210902f4af79
anthonyargatoff <anthonyargatoff@gmail.com>
2025-02-16 16:10:58 -0800

    Remove vacancy type

---
+++ db.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+
+def createSchema():
+    try:
+        con = sqlite3.connect("toronto.db")
+        cur = con.cursor()
+
+        cur.executescript(
+            """
+            CREATE TABLE coop(
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                availability INTEGER,
+            );
+
+            CREATE TABLE statistics(
+                id INTEGER PRIMARY KEY,
+                totalCount INTEGER,
+                weeklyCount INTEGER
+            );
+            """
+        )
+    except Exception as error:
+        print(error)
+
+
+if __name__ == "__main__":
+    createSchema()
