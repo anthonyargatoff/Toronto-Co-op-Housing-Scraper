01d3310d9905f106ff1dbc81ba87d816ec724972
anthonyargatoff <anthonyargatoff@gmail.com>
2025-02-17 16:06:50 -0800

    ad print to main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from compareVacancies import compareVacancies
 from adminReport import adminReport
     
+print("Scheduler starting")
 schedule.every(10).minutes.do(compareVacancies)
 schedule.every().sunday.at("08:00").do(adminReport)
 
